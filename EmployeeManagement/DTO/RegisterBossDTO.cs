da30f3ebc57cd72d29be2f6405004a9f49cea39f
wicekv <lukaszwickowski@wp.pl>
2022-04-06 22:28:35 +0200

    Added DTO for register boss or employee

---
+++ EmployeeManagement/DTO/RegisterBossDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.DTO
+{
+    public class RegisterBossDTO
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int RoleId { get; set; }
+        public string UniqueCode { get; set; }
+        public string NIP { get; set; }
+        public string CompanyName { get; set; }
+        public string Code { get; set; }
+    }
+}
---
+++ EmployeeManagement/DTO/RegisterEmployeeDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.DTO
+{
+    public class RegisterEmployeeDTO
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int RoleId { get; set; }
+        public int PhoneNumber { get; set; }
+        public int CompanyId { get; set; }
+    }
+}
