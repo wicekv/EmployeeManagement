faccb58cf3ad65dd1f24864b7a7f75ef4ca79798
wicekv <lukaszwickowski@wp.pl>
2022-04-09 11:28:40 +0200

    Revert "Added automapper"
    This reverts commit fcaf8f5377fc4a3d86ccd4f4c0feb3d40cd8b636.

--- EmployeeManagement/Controllers/HomeController.cs
+++ EmployeeManagement/Controllers/HomeController.cs
@@ -1,6 +1,4 @@
-﻿using EmployeeManagement.DTO;
-using EmployeeManagement.Models;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -9,41 +7,9 @@ using System.Threading.Tasks;
 
 namespace EmployeeManagement.Controllers
 {
-    [Route("api/employee")]
+    [Route("api/[controller]")]
     [ApiController]
     public class HomeController : ControllerBase
     {
-        private readonly DbEmployeeManagementContext dbEmployeeManagementContext;
-
-        public HomeController(DbEmployeeManagementContext dbEmployeeManagementContext)
-        {
-            this.dbEmployeeManagementContext = dbEmployeeManagementContext;
-        }
-
-        [HttpGet]
-        public ActionResult<IEnumerable<Employee>> GetEmployees()
-        {
-            var employees = dbEmployeeManagementContext
-                .Employees
-                .ToList();
-
-            return Ok(employees);
-        }
-
-        [HttpGet("{id}")]
-        public ActionResult<Employee> GetEmployee([FromRoute] int id)
-        {
-            var employee = dbEmployeeManagementContext
-                .Employees
-                .FirstOrDefault(e => e.UserId == id);
-
-            if(employee is null)
-            {
-                return NotFound("This employee is not exist");
-            }else
-            {
-                return Ok(employee);
-            }
-        }
     }
 }
