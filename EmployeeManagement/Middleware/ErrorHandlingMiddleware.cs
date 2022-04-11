eb3a0908d3abf1a7885b1d7df8c79654460a26fb
wicekv <lukaszwickowski@wp.pl>
2022-04-11 16:07:01 +0200

    Error HandlingMiddleware

---
+++ EmployeeManagement/Middleware/ErrorHandlingMiddleware.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Middleware
+{
+    public class ErrorHandlingMiddleware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                await next.Invoke(context);
+            }
+            catch(Exception e)
+            {
+
+            }
+        }
+    }
+}
